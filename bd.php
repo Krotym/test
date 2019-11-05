793a3b58578468ee1c80a4921980df376b6baa8a
Krotym <55706673+Krotym@users.noreply.github.com>
2019-11-05 17:05:53 +0200

    Update bd.php

--- bd.php
+++ bd.php
@@ -7,29 +7,13 @@ $host = 'localhost';
   $pass = '1410261';
   $db_name = 'student';
   $link = mysqli_connect($host, $user, $pass, $db_name);
-  $che=0;
+ 
   
   
   if (!$link) {
     echo 'code err: ' . mysqli_connect_errno() . ', err: ' . mysqli_connect_error();
     exit;
 }
-if($che==0){
-//создание таблици
-$query  ="CREATE Table tovars
-(
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(200) NOT NULL,
-    price VARCHAR(200) NOT NULL,
-    text VARCHAR(255) NOT NULL
-)";
-$result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link)); 
-if($result)
-{
-    echo "Создание таблицы прошло успешно";
-}
-    $che++;
-  }
   
 $name = htmlentities(mysqli_real_escape_string($link, $_POST['name']));
 $price = htmlentities(mysqli_real_escape_string($link, $_POST['price']));
